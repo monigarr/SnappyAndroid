4d6f65ea0cf8c323d9b8c9e148470c64197d5fa4
MONICA PETERS <monigarr@yahoo.com>
2014-02-15 17:44:30 -0500

    Refactored Fragments for the Navigation tabs into its own new class

--- gen/com/monigarr/snappyandroid/R.java
+++ gen/com/monigarr/snappyandroid/R.java
@@ -50,25 +50,24 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int email_hint=0x7f05000d;
-        public static final int hello_world=0x7f050006;
-        public static final int login_button_label=0x7f05000a;
-        public static final int login_error_message=0x7f050012;
-        public static final int login_error_title=0x7f050013;
-        public static final int login_text=0x7f05000f;
-        public static final int menu_logout_label=0x7f050014;
-        public static final int password_hint=0x7f050009;
-        public static final int sign_up_text=0x7f05000b;
-        public static final int signup_button_label=0x7f05000e;
-        public static final int signup_error_message=0x7f050010;
-        public static final int signup_error_title=0x7f050011;
-        public static final int title_activity_login=0x7f050005;
-        public static final int title_activity_sign_up=0x7f05000c;
-        public static final int title_activity_sing_up=0x7f050007;
+        public static final int email_hint=0x7f05000c;
+        public static final int hello_world=0x7f050005;
+        public static final int login_button_label=0x7f050009;
+        public static final int login_error_message=0x7f050011;
+        public static final int login_error_title=0x7f050012;
+        public static final int login_text=0x7f05000e;
+        public static final int menu_logout_label=0x7f050013;
+        public static final int password_hint=0x7f050008;
+        public static final int sign_up_text=0x7f05000a;
+        public static final int signup_button_label=0x7f05000d;
+        public static final int signup_error_message=0x7f05000f;
+        public static final int signup_error_title=0x7f050010;
+        public static final int title_activity_login=0x7f050004;
+        public static final int title_activity_sign_up=0x7f05000b;
+        public static final int title_activity_sing_up=0x7f050006;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
-        public static final int title_section3=0x7f050004;
-        public static final int username_hint=0x7f050008;
+        public static final int username_hint=0x7f050007;
     }
     public static final class style {
         /** 
--- src/com/monigarr/snappyandroid/MainActivity.java
+++ src/com/monigarr/snappyandroid/MainActivity.java
@@ -1,15 +1,11 @@
 package com.monigarr.snappyandroid;
 
-import java.util.Locale;
-
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -62,7 +58,7 @@ public class MainActivity extends FragmentActivity implements
 
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
+		mSectionsPagerAdapter = new SectionsPagerAdapter(this, 
 				getSupportFragmentManager());
 
 		// Set up the ViewPager with the sections adapter.
@@ -136,48 +132,6 @@ public class MainActivity extends FragmentActivity implements
 			FragmentTransaction fragmentTransaction) {
 	}
 
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Fragment fragment = new DummySectionFragment();
-			Bundle args = new Bundle();
-			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 3;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_section3).toUpperCase(l);
-			}
-			return null;
-		}
-	}
 
 	/**
 	 * A dummy fragment representing a section of the app, but that simply
