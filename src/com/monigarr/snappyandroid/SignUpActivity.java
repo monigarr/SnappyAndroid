469c24cac65d440a13647ce6e847224b216e1c04
MONICA PETERS <monigarr@yahoo.com>
2014-02-15 15:35:09 -0500

    email verification added for SignUpActivity

--- src/com/monigarr/snappyandroid/SignUpActivity.java
+++ src/com/monigarr/snappyandroid/SignUpActivity.java
@@ -2,11 +2,16 @@ package com.monigarr.snappyandroid;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends Activity {
 
@@ -45,6 +50,31 @@ public class SignUpActivity extends Activity {
 					dialog.show();
 				} else {
 					// create new user
+					ParseUser newUser = new ParseUser();
+					newUser.setUsername(username);
+					newUser.setPassword(password);
+					newUser.setEmail(email);
+					newUser.signUpInBackground(new SignUpCallback() {
+						
+						@Override
+						public void done(ParseException e) {
+							if (e == null){
+								//success
+								Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+								intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+								intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+								startActivity(intent);
+							} else {
+								AlertDialog.Builder builder = new AlertDialog.Builder(
+										SignUpActivity.this);
+								builder.setMessage(e.getMessage())
+										.setTitle(R.string.signup_error_title)
+										.setPositiveButton(android.R.string.ok, null);
+								AlertDialog dialog = builder.create();
+								dialog.show();
+							}
+						}
+					});
 				}
 			}
 		});
