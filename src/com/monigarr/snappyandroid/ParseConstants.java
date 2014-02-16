bb6440ccc4b01a5a7e928b7e6d4c889b1d5572a1
MONICA PETERS <monigarr@yahoo.com>
2014-02-16 14:13:43 -0500

    creating the message to send media to parse.com db and to friends, not done yet just a few more tasks to complete in a few minutes

--- src/com/monigarr/snappyandroid/ParseConstants.java
+++ src/com/monigarr/snappyandroid/ParseConstants.java
@@ -3,8 +3,14 @@ package com.monigarr.snappyandroid;
 public final class ParseConstants {
 
 	//Class Names
+	public static final String CLASS_MESSAGES = "Messages";
 	
 	//Field Names
 	public static final String KEY_USERNAME = "username";
 	public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+	public static final String KEY_RECIPIENT_IDS = "recipientIds";
+	public static final String KEY_SENDER_ID = "senderId";
+	public static final String KEY_SENDER_NAME = "senderName";
+	public static final String KEY_FILE = "file";
+	public static final String KEY_FILE_TYPE = "fileType";
 }
--- src/com/monigarr/snappyandroid/RecipientsActivity.java
+++ src/com/monigarr/snappyandroid/RecipientsActivity.java
@@ -1,5 +1,6 @@
 package com.monigarr.snappyandroid;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.AlertDialog;
@@ -16,6 +17,7 @@ import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
@@ -114,6 +116,8 @@ public class RecipientsActivity extends ListActivity {
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		case R.id.action_send:
+			ParseObject message = createMessage();
+			//send(message);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -124,8 +128,27 @@ public class RecipientsActivity extends ListActivity {
 		super.onListItemClick(l, v, position, id);
 		if (l.getCheckedItemCount() > 0) {
 			mSendMenuItem.setVisible(true);
+		} else {
+			mSendMenuItem.setVisible(false);
 		}
-		mSendMenuItem.setVisible(false);
+	}
+	protected ParseObject createMessage() {
+		ParseObject message = new ParseObject(ParseConstants.CLASS_MESSAGES);
+		message.put(ParseConstants.KEY_SENDER_ID, ParseUser.getCurrentUser().getObjectId());
+		message.put(ParseConstants.KEY_SENDER_NAME, ParseUser.getCurrentUser().getUsername());
+		message.put(ParseConstants.KEY_RECIPIENT_IDS, getRecipientIds());
+		
+		return message;
+	}
+	
+	protected ArrayList<String> getRecipientIds() {
+		ArrayList<String> recipientIds = new ArrayList<String>();
+		for (int i = 0; i < getListView().getCount(); i++) {
+			if (getListView().isItemChecked(i)) {
+				recipientIds.add(mFriends.get(i).getObjectId());
+			}
+		}
+		return recipientIds;
 	}
 	
 }
