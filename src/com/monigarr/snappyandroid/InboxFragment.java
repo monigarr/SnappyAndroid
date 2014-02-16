f240500764c75699771117126339e845238408e5
MONICA PETERS <monigarr@yahoo.com>
2014-02-16 18:20:50 -0500

    added custom list adapter for the messages to show either image or video icon to match message type

--- src/com/monigarr/snappyandroid/InboxFragment.java
+++ src/com/monigarr/snappyandroid/InboxFragment.java
@@ -2,18 +2,21 @@ package com.monigarr.snappyandroid;
 
 import java.util.List;
 
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 public class InboxFragment extends ListFragment {
 
@@ -25,8 +28,6 @@ public class InboxFragment extends ListFragment {
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_inbox,
 				container, false);
-
-
 		return rootView;
 	}
 	
@@ -52,14 +53,30 @@ public class InboxFragment extends ListFragment {
 						usernames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
 						i++;
 					}
-					ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-							getListView().getContext(), 
-							android.R.layout.simple_list_item_1, 
-							usernames);
+					MessageAdapter adapter = new MessageAdapter(getListView().getContext(), mMessages);
 					setListAdapter(adapter);
 				}
 			}
 		});
 	}
 	
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		ParseObject message = mMessages.get(position);
+		String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+		ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
+		Uri fileUri = Uri.parse(file.getUrl());
+		
+		if (messageType.equals(ParseConstants.TYPE_IMAGE)) {
+			// view the image
+			Intent intent = new Intent(getActivity(), ViewImageActivity.class);
+			intent.setData(fileUri);
+			startActivity(intent);
+		}
+		else {
+			// view the video
+		}
+	}
 }
