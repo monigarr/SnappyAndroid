f0148bf6afcedd95b831e97c26dc4d0fe4b2c648
MONICA PETERS <monigarr@yahoo.com>
2014-02-16 15:00:45 -0500

    we can add file to a message now

--- src/com/monigarr/snappyandroid/RecipientsActivity.java
+++ src/com/monigarr/snappyandroid/RecipientsActivity.java
@@ -18,6 +18,7 @@ import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
@@ -144,7 +145,22 @@ public class RecipientsActivity extends ListActivity {
 		message.put(ParseConstants.KEY_SENDER_NAME, ParseUser.getCurrentUser().getUsername());
 		message.put(ParseConstants.KEY_RECIPIENT_IDS, getRecipientIds());
 		message.put(ParseConstants.KEY_FILE_TYPE, mFileType);
-		return message;
+		
+		byte[] fileBytes = FileHelper.getByteArrayFromFile(this, mMediaUri);
+		
+		if (fileBytes == null) {
+			//problem
+			return null;
+		} else {
+			if (mFileType.equals(ParseConstants.TYPE_IMAGE)) {
+				fileBytes = FileHelper.reduceImageForUpload(fileBytes);
+			}
+			
+			String fileName = FileHelper.getFileName(this, mMediaUri, mFileType);
+			ParseFile file = new ParseFile(fileName, fileBytes);
+			message.put(ParseConstants.KEY_FILE, file);
+			return message;
+		}
 	}
 	
 	protected ArrayList<String> getRecipientIds() {
