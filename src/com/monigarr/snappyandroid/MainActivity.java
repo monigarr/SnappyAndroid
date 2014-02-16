bf7d0e1b84ce7363d004e3004bdd3a91e588c6d2
MONICA PETERS <monigarr@yahoo.com>
2014-02-15 19:57:58 -0500

    Friends show on FriendsActivity list now

--- src/com/monigarr/snappyandroid/MainActivity.java
+++ src/com/monigarr/snappyandroid/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
@@ -36,6 +37,7 @@ public class MainActivity extends FragmentActivity implements
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_main);
 		
 		ParseAnalytics.trackAppOpened(getIntent());
